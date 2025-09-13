478b8efdf1bab486e8b5e067dd5fc303301ae9a9
Grant Barnes <gfbarnes78@gmail.com>
2025-09-13 16:43:11 -0500

    infrastructure - only ask for db backup if droplet is found

--- delete.sh
+++ delete.sh
@@ -11,64 +11,6 @@ APP_NAME="card-judge"
 DROPLET_NAME="$APP_NAME-database"
 
 ################################################################################
-# backup database
-
-read -p "Do you want to backup the database? [Y/n]: " BACKUP_DB
-if [[ "$BACKUP_DB" != "n" ]]; then
-	echo "----------------------------------------"
-	echo "Backing Up Database..."
-
-	BACKUP_SQL_PATH="$(pwd)/backups/$(date +%Y%m%d%H%M%S)_backup_card_judge.sql"
-
-	DROPLET_IP=$(doctl compute droplet list --format=PublicIPv4,Name --no-header | grep $DROPLET_NAME | cut -d ' ' -f 1)
-	if [[ -z "$DROPLET_IP" ]]; then
-		echo "Droplet IP not found"
-		exit 1
-	fi
-
-	ssh root@$DROPLET_IP 'mariadb-dump --order-by-primary CARD_JUDGE | sed -e '\''s/DEFINER[ ]*=[ ]*[^*]*\*/\*/'\'' > /root/backup.sql'
-	scp root@$DROPLET_IP:/root/backup.sql "$BACKUP_SQL_PATH" >/dev/null 2>&1
-
-	if [ ! -f "$BACKUP_SQL_PATH" ]; then
-		echo "Backup failed: backup file not found"
-		exit 1
-	fi
-
-	if [ ! -s "$BACKUP_SQL_PATH" ]; then
-		echo "Backup failed: backup file is empty"
-		exit 1
-	fi
-
-	if find "$BACKUP_SQL_PATH" -mmin +1 -print -quit | grep -q .; then
-		echo "Backup failed: backup file is older than 1 minute"
-		exit 1
-	fi
-
-	BACKUP_SQL_SIZE=$(stat -c%s "$BACKUP_SQL_PATH")
-	if (( $BACKUP_SQL_SIZE < 1024 )); then
-		echo "Backup failed: backup file is too small"
-		exit 1
-	fi
-
-	BACKUP_SQL_LAST_LINE=$(tail -n 1 "$BACKUP_SQL_PATH")
-	if ! [[ "$BACKUP_SQL_LAST_LINE" =~ ^"-- Dump completed on " ]]; then
-		echo "Backup failed: backup file does not appear to be valid"
-		exit 1
-	fi
-
-	BACKUP_GPG_PATH="$BACKUP_SQL_PATH".gpg
-	rm -f $BACKUP_GPG_PATH
-	gpg -c --output $BACKUP_GPG_PATH $BACKUP_SQL_PATH
-
-	if [ ! -f "$BACKUP_GPG_PATH" ]; then
-		echo "File not found: $BACKUP_GPG_PATH"
-		exit 1
-	fi
-
-	echo "Database Backed Up"
-fi
-
-################################################################################
 # delete droplet
 
 echo "----------------------------------------"
@@ -78,6 +20,60 @@ DROPLET_ID=$(doctl compute droplet list --format=ID,Name --no-header | grep $DRO
 if [[ -z "$DROPLET_ID" ]]; then
 	echo "Droplet ID not found"
 else
+	read -p "Do you want to backup the database? [Y/n]: " BACKUP_DB
+	if [[ "$BACKUP_DB" != "n" ]]; then
+		echo "----------------------------------------"
+		echo "Backing Up Database..."
+
+		BACKUP_SQL_PATH="$(pwd)/backups/$(date +%Y%m%d%H%M%S)_backup_card_judge.sql"
+
+		DROPLET_IP=$(doctl compute droplet list --format=PublicIPv4,Name --no-header | grep $DROPLET_NAME | cut -d ' ' -f 1)
+		if [[ -z "$DROPLET_IP" ]]; then
+			echo "Droplet IP not found"
+			exit 1
+		fi
+
+		ssh root@$DROPLET_IP 'mariadb-dump --order-by-primary CARD_JUDGE | sed -e '\''s/DEFINER[ ]*=[ ]*[^*]*\*/\*/'\'' > /root/backup.sql'
+		scp root@$DROPLET_IP:/root/backup.sql "$BACKUP_SQL_PATH" >/dev/null 2>&1
+
+		if [ ! -f "$BACKUP_SQL_PATH" ]; then
+			echo "Backup failed: backup file not found"
+			exit 1
+		fi
+
+		if [ ! -s "$BACKUP_SQL_PATH" ]; then
+			echo "Backup failed: backup file is empty"
+			exit 1
+		fi
+
+		if find "$BACKUP_SQL_PATH" -mmin +1 -print -quit | grep -q .; then
+			echo "Backup failed: backup file is older than 1 minute"
+			exit 1
+		fi
+
+		BACKUP_SQL_SIZE=$(stat -c%s "$BACKUP_SQL_PATH")
+		if (( $BACKUP_SQL_SIZE < 1024 )); then
+			echo "Backup failed: backup file is too small"
+			exit 1
+		fi
+
+		BACKUP_SQL_LAST_LINE=$(tail -n 1 "$BACKUP_SQL_PATH")
+		if ! [[ "$BACKUP_SQL_LAST_LINE" =~ ^"-- Dump completed on " ]]; then
+			echo "Backup failed: backup file does not appear to be valid"
+			exit 1
+		fi
+
+		BACKUP_GPG_PATH="$BACKUP_SQL_PATH".gpg
+		rm -f $BACKUP_GPG_PATH
+		gpg -c --output $BACKUP_GPG_PATH $BACKUP_SQL_PATH
+
+		if [ ! -f "$BACKUP_GPG_PATH" ]; then
+			echo "File not found: $BACKUP_GPG_PATH"
+			exit 1
+		fi
+
+		echo "Database Backed Up"
+	fi
 	doctl compute droplet delete $DROPLET_ID --force
 	echo "Droplet Deleted"
 fi
